09c1e875bc2c003f96bdffd1c01ac5cf99fe64a6
Dave <daveddevel@gmail.com>
2024-08-31 11:49:26 -0300

    [CBM] - Initial commit.

---
+++ src/main/com/assessment/ContactBookManagement.java
@@ -0,0 +1,9 @@
+package com.assessment;
+
+class AssessmentTemplate {
+
+    public static void main(String... args){
+        System.out.println("Ready!");
+    }
+
+}
---
+++ src/test/com/assessment/ContactBookManagementTest.java
@@ -0,0 +1,13 @@
+package com.assessment;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ContactBookManagementTest {
+
+    @Test
+    public void testAssessment(){
+        Assertions.assertTrue(true);
+    }
+
+}
